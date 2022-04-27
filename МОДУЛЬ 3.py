4040af30cf5490675e544dca22aaf1e2518db083
хороший человек <yndolokop>
2022-04-27 04:06:36 +0100

    Функции в Пайтон

--- МОДУЛЬ 3.py
+++ МОДУЛЬ 3.py
@@ -1,12 +1,17 @@
-
-history_item = []  #  список товаров
-history_price = []  # список цен
+'''
+Программа позволяет повторять многократно счет и вычитает цену товара из баланса на счету.
+При повторном пополнении баланса после совершения покупок, суммирует остаток с новым пополнением.
+Выводит историю покупок.
+Не смог написать без введения глобальных переменных.
+'''
+history_item = []  #  хранит список товаров
+history_price = []  # хранит список цен
 balance_sum = []  #  хранит суммы пополнений
+result = 0
 
 
 def main_menu():
     global result
-    result = 0
     while True:
         print('1. пополнение счета')
         print('2. покупка')
@@ -15,22 +20,21 @@ def main_menu():
         choice = input('Выберите пункт меню: ')
         if choice == '1':
             balance = int(input('Пополните счет. Введите сумму пополнения: '))
-            balance_sum.clear()
-            balance_sum.append(result)
-            print('Ваш баланс: ')
-            result = bal(balance)
+            balance_sum.clear()  # очищаем список от ранее добавленных значений
+            balance_sum.append(result)  # и вносим result, то что осталось после совершения покупок
+            result = bal(balance)  # присвоим переменной result значение которая вернула функция
+            print('Ваш баланс: ', result)
         elif choice == '2':
             price = int(input('Введите сумму покупки: '))
             if price > result:
                 print('Недостаточно денег. Пополните счет')
             elif price <= result:
                 item_name = input('Введите название товара: ')
-                result = result - price  # и вычитать из нее сумму покупок перезаписав в result
-                print(balance_sum)
+                result = result - price
                 print('Остаток на счету: ', result)
                 history_item.append(item_name)
                 history_price.append(price)
-                print('Список покупок:\n', history_item)
+                print('Список покупок: ', history_item)
             else:
                 print('Обратитесь в техподдержку')
         elif choice == '3':
@@ -46,14 +50,13 @@ def bal(val):  # Функция будет принимать переменну
     new_balance = 0
     balance_sum.append(val)
     for i in balance_sum:
-        new_balance += i  # суммировать их в переменную new_balance
-    print(new_balance)
+        new_balance += i  # суммировать в переменную new_balance
     return new_balance
 
 
-def shopping_history():  #  функция печатает историю покупок из двух списков: названий товара и цен
+def shopping_history():  # функция печатает историю покупок из двух списков: названий товара и цен
     dict_history = dict(zip(history_item, history_price))
-    print('История покупок:\n', dict_history)
+    print('История покупок: ', dict_history)
 
 
 main_menu()
