efab633b4d601665fe4213a1dea6401b72eb4886
хороший человек <yndolokop>
2022-04-28 01:57:27 +0100

    Функции в Пайтон

--- МОДУЛЬ 1(работа над ошибками).py
+++ МОДУЛЬ 1(работа над ошибками).py
@@ -1,13 +1,24 @@
-def simple_separator():
-    """
-    Функция создает красивый резделитель из 10-и звездочек (**********)
-    :return: **********
-    """
-    x = '*' * 10
-    return x
+"""
+МОДУЛЬ 1
+В модуле прописаны заготовки для 8 функций
+Под каждой функцией есть описание как она должна работать
+ниже есть примеры использования функции
+Задание: реализовать код функции, чтобы он работал по описанию и примеры использования давали верный результат
+"""
 
 
-print(simple_separator() == '**********')  # True
+def separator(simbol, count):
+    """
+    Функция создает разделитель из любых символов любого количества
+    :param simbol: символ разделителя
+    :param count: количество повторений
+    :return: строка разделитель примеры использования ниже
+    """
+    return simbol * count
+
+
+print(separator('-', 10) == '----------')  # True
+print(separator('#', 5) == '#####')  # True
 
 
 def long_separator(count):
@@ -16,92 +27,77 @@ def long_separator(count):
     :param count: количество звездочек
     :return: строка разделитель, примеры использования ниже
     """
-    x = '*' * count
-    return x
+    return separator('*', count)
 
 
 print(long_separator(3) == '***')  # True
 print(long_separator(4) == '****')  # True
 
 
-def separator(simbol, count):
+def simple_separator():
     """
-        Функция создает разделитель из любых символов любого количества
-        :param simbol: символ разделителя
-        :param count: количество повторений
-        :return: строка разделитель примеры использования ниже
-        """
-    x = simbol * count
-    return x
-
-
-print(separator('-', 10) == '----------')  # True
-print(separator('#', 5) == '#####')  # True
-
-
-def hello_who(who='World!'):
+    Функция создает красивый резделитель из 10-и звездочек (**********)
+    :return: **********
     """
-    Функция печатает приветствие в красивом формате
-    **********
+    return long_separator(10)
 
-    Hello {who}!
 
-    ##########
-    :param who: кого мы приветствуем, по умолчанию World
-    :return: None
-    """
-    separator('*', 10)
-    print()
-    print('Hello ' f'{who}')
-    print()
-    separator('#', 10)
-'''
-**********
-
-Hello World!
-
-##########
-'''
-hello_who()
+print(simple_separator() == '**********')  # True
 
 
 def hello_world():
     """
     Функция печатает Hello World в формате:
     **********
-
     Hello World!
-
     ##########
     :return: None
     """
-    hello_who()
-
+    separator('*', 10)
+    print()
+    print('Hello World!')
+    print()
+    separator('#', 10)
 '''
 **********
-
 Hello World!
-
 ##########
 '''
 hello_world()
+
+
+def hello_who(who='World!'):
+    """
+    Функция печатает приветствие в красивом формате
+    **********
+    Hello {who}!
+    ##########
+    :param who: кого мы приветствуем, по умолчанию World
+    :return: None
+    """
+    separator('*', 10)
+    print()
+    print(f'Hello {who}')
+    print()
+    separator('#', 10)
 '''
 **********
-
+Hello World!
+##########
+'''
+hello_who()
+'''
+**********
 Hello Max!
-
 ##########
 '''
 hello_who('Max')
 '''
 **********
-
 Hello Kate!
-
 ##########
 '''
 hello_who('Kate')
-
 
 
 def pow_many(power, *args):
@@ -123,7 +119,6 @@ print(pow_many(1, 2, 3) == 5)  # True -> (2 + 3)**1 == 5
 print(pow_many(2, 1, 1) == 4)  # True -> (1 + 1)**2 == 4
 print(pow_many(3, 2) == 8)  # True -> 2**3 == 8
 print(pow_many(2, 1, 2, 3, 4) == 100)  # True -> (1 + 2 + 3 + 4)**2 == 10**2 == 100
-print()
 
 
 def print_key_val(**kwargs):
@@ -143,14 +138,11 @@ name --> Max
 age --> 21
 """
 print_key_val(name='Max', age=21)
-print()
 """
 animal --> Cat
 is_animal --> True
 """
 print_key_val(animal='Cat', is_animal=True)
-print()
-
 
 
 def my_filter(iterable, function):
@@ -159,11 +151,14 @@ def my_filter(iterable, function):
     Функция фильтрует последовательность iterable и возвращает новую
     Если function от элемента последовательности возвращает True, то элемент входит в новую последовательность иначе нет
     (примеры ниже)
-    :param iterable: входная последовательность
+    :param iterable: входаня последовательности
     :param function: функция фильтрации
     :return: новая отфильтрованная последовательность
     """
-    result = list(filter(function, iterable))
+    result = []
+    for item in iterable:
+        if function(item):
+            result.append(item)
     return result
 
 
@@ -171,5 +166,3 @@ print(my_filter([1, 2, 3, 4, 5], lambda x: x > 3) == [4, 5])  # True
 print(my_filter([1, 2, 3, 4, 5], lambda x: x == 2) == [2])  # True
 print(my_filter([1, 2, 3, 4, 5], lambda x: x != 3) == [1, 2, 4, 5])  # True
 print(my_filter(['a', 'b', 'c', 'd'], lambda x: x in 'abba') == ['a', 'b'])  # True
-
-
